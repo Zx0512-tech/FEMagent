1904e462ed8c48ba05148afe10b9aee7f705d633
Xuan Zhou <1546811605@qq.com>
2026-09-05 14:06:27 +0800

    feat(pr13): export role evidence projection

--- fem_core/semantic_roles/__init__.py
+++ fem_core/semantic_roles/__init__.py
@@ -1,10 +1,12 @@
 """Deterministic engineering semantic-role resolution."""
 
 from .api import inspect_semantic_roles, resolve_semantic_role
+from .evidence import project_role_evidence
 from .manifest import load_semantic_manifest
 
 __all__ = [
     "inspect_semantic_roles",
     "load_semantic_manifest",
+    "project_role_evidence",
     "resolve_semantic_role",
 ]
