184150f50403c525fb7a7e38504689b58b87c4ab
Xuan Zhou <1546811605@qq.com>
2026-09-07 21:38:03 +0800

    feat: export Engineering Analysis Readiness

--- fem_core/analysis_spec/__init__.py
+++ fem_core/analysis_spec/__init__.py
@@ -1,3 +1,7 @@
+from fem_core.analysis_spec.readiness import evaluate_engineering_analysis_readiness
 from fem_core.analysis_spec.validator import validate_engineering_analysis_spec
 
-__all__ = ["validate_engineering_analysis_spec"]
+__all__ = [
+    "evaluate_engineering_analysis_readiness",
+    "validate_engineering_analysis_spec",
+]
