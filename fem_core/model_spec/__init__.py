c5e98b69466aa945903b6a7f33cb8fc9a2a401f0
Xuan Zhou <1546811605@qq.com>
2026-09-07 12:58:00 +0800

    feat: export engineering model readiness API

--- fem_core/model_spec/__init__.py
+++ fem_core/model_spec/__init__.py
@@ -1,3 +1,4 @@
+from fem_core.model_spec.readiness import evaluate_engineering_model_readiness
 from fem_core.model_spec.validator import validate_engineering_model_spec
 
-__all__ = ["validate_engineering_model_spec"]
+__all__ = ["evaluate_engineering_model_readiness", "validate_engineering_model_spec"]
