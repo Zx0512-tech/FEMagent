23901406c9f6a8ecd4a41d4e6ad79ee6a25b7cb2
Xuan Zhou <1546811605@qq.com>
2026-09-10 10:41:52 +0800

    feat: export OpenSees analysis profiles

---
+++ fem_core/analysis_spec/opensees_profiles/__init__.py
@@ -0,0 +1,17 @@
+from fem_core.analysis_spec.opensees_profiles.registry import (
+    OPENSEES_MODAL_V2,
+    OPENSEES_STATIC_V1,
+    OPENSEES_STATIC_V2,
+    OPENSEES_TRANSIENT_BASE_V2,
+    OPENSEES_TRANSIENT_NODAL_V2,
+    select_opensees_analysis_profile,
+)
+
+__all__ = [
+    "OPENSEES_MODAL_V2",
+    "OPENSEES_STATIC_V1",
+    "OPENSEES_STATIC_V2",
+    "OPENSEES_TRANSIENT_BASE_V2",
+    "OPENSEES_TRANSIENT_NODAL_V2",
+    "select_opensees_analysis_profile",
+]
