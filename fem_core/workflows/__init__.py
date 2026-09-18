69142b08d5aef8eed02ad4620710641319cb11ee
Xuan Zhou <1546811605@qq.com>
2026-09-18 22:27:35 +0800

    feat: export PR31 earthquake workflow APIs

---
+++ fem_core/workflows/__init__.py
@@ -0,0 +1,15 @@
+from fem_core.workflows.earthquake import (
+    WORKFLOW_PREPARATION_SCHEMA,
+    WORKFLOW_SCHEMA,
+    WORKFLOW_SUMMARY_SCHEMA,
+    prepare_earthquake_workflow,
+    summarize_earthquake_workflow,
+)
+
+__all__ = [
+    "WORKFLOW_PREPARATION_SCHEMA",
+    "WORKFLOW_SCHEMA",
+    "WORKFLOW_SUMMARY_SCHEMA",
+    "prepare_earthquake_workflow",
+    "summarize_earthquake_workflow",
+]
