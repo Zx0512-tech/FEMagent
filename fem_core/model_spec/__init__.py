0d97d6ffe9e7820f33986839f84b606329eeb7b9
Xuan Zhou <1546811605@qq.com>
2026-09-20 18:38:14 +0800

    feat: export deterministic ANSYS model renderer

--- fem_core/model_spec/__init__.py
+++ fem_core/model_spec/__init__.py
@@ -1,9 +1,15 @@
+from fem_core.model_spec.ansys_renderer import (
+    render_ansys_frame_2d,
+    verify_ansys_model_render,
+)
 from fem_core.model_spec.opensees_renderer import render_opensees_frame_2d
 from fem_core.model_spec.readiness import evaluate_engineering_model_readiness
 from fem_core.model_spec.validator import validate_engineering_model_spec
 
 __all__ = [
     "evaluate_engineering_model_readiness",
+    "render_ansys_frame_2d",
     "render_opensees_frame_2d",
     "validate_engineering_model_spec",
+    "verify_ansys_model_render",
 ]
